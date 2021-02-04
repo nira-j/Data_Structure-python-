78df9afeaa4042146385e490e91ce23473d03e98
niraj <niraj0968@gmail.com>
2021-02-04 11:16:12 +0530

    first commit add comments

--- bubble sort.py
+++ bubble sort.py
@@ -1,8 +1,13 @@
 # bubble sorting
+# Time complexity for bubble sort is O(n^2) ad sace complexity is O(n).
+# Bubble sort does not take extra space for sorting elements of list.
 
+# List of items to be sorted 
 items=[12,22,11,1,2,3,4,55,6,7,8,90,7,6,55,44,34,576,33,56,234,567,890,111,223,445,678,84]
 print(items)
 length=len(items)
+# len() method returns number of elements in a list
+ 
 for i in range(length-1):
     for j in range(length-i-1):
         if(items[j] > items[j+1]):
--- selection_sort.py
+++ selection_sort.py
@@ -1,8 +1,12 @@
 # selection sorting
+# Time complexity of selection sort is O(n^2) While space complexity is O(n).
+# selection sort does not take extra memory space to sort elements in a list.
 
+# list of item s to be sort
 items=[12,22,11,1,2,3,4,55,6,7,8,90,7,234,567,890,111,223,445,678,84]
 print(items)
 length=len(items)
+# len() return length of list
 for i in range(length):
     min_index=i
     for j in range(i+1,length):
@@ -12,3 +16,4 @@ for i in range(length):
     items[i],items[min_index]=items[min_index],items[i]
 print()
 print(items)
+
